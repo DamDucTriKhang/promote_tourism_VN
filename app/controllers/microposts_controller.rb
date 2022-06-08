48699d9ba52b2f14cccaae129523dd220a93dd2e
DamDucTriKhang <khangddt@rubyonring.com>
2022-06-08 14:08:59 +0700

    update_sigup

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,5 @@
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: %i[ show edit update destroy ]
-  before_action :new_micropost, only: %i[ new create ]
 
   def index
     @microposts = Micropost.all
