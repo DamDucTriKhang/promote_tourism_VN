6911e5da87e5bbeea0bb606ab194869097d490b8
DamDucTriKhang <khangddt@rubyonring.com>
2022-06-15 14:18:18 +0700

    edit_delete_microposts_user

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,5 @@
 class MicropostsController < ApplicationController
+  before_action :find_micropost, only: [:edit, :update]
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,only: :destroy
 
@@ -18,13 +19,31 @@ class MicropostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    user = User.find_by_id(params[:micropost][:user_id])
+    if @micropost.update(micropost_params)
+      flash[:success] = "micropost updated!"
+      redirect_to user_path(user)
+    else
+      flash[:danger] = "micropost no update!"
+      render :edit
+    end
+  end
+
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to request.referrer || root_url
+    redirect_to request.referrer || micropost_path
   end
 
   private
+
+  def find_micropost
+    @micropost = Micropost.find_by(id: params[:id])
+  end
 
   def micropost_params
     params.require(:micropost).permit(:content, :image)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,10 @@ module SessionsHelper
     current_user
   end
 
+  def current_user?(user)
+    user && user == current_user
+  end
+
 	def log_out
     forget(current_user)
     session.delete(:user_id)
